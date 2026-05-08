9ee2a4bfba18ab7a570a402acb124fa9c8295f85
JanMaki <mutsukimaki@gmail.com>
2026-05-08 13:43:12 +0900

    Add bcdiceパッケージを追加

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono'
+import {Version} from "bcdice";
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text(`BCDice Version: ${Version}`)
 })
 
 export default app
